f0320662d8d83caaf15b8db6d21e27a1207ce1cd
TaoXuSheng <xusheng.tao@ele.me>
2018-07-04 15:49:28 +0800

    change build config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
-// import { minify } from 'uglify-es'
+import { minify } from 'uglify-es'
 import babel from 'rollup-plugin-babel'
-// import uglify from 'rollup-plugin-uglify'
+import uglify from 'rollup-plugin-uglify'
 import eslint from 'rollup-plugin-eslint'
-import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
-    name: 'StylusToAST',
+    name: 'FocusOutside',
     format: 'umd'
   },
   plugins: [
-    commonjs(),
-    resolve(),
     eslint({
       throwError: true,
       exclude: 'node_modules/**'
+    }),
+    resolve({
+      extensions: ['.js']
     }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers']
     })
+    uglify({}, minify)
   ]
 }
