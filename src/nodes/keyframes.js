1db69569b122b8b9b88033733eb41d4a5102548c
mac126 <>
2018-07-11 17:35:36 +0800

    add keyframes node.

---
+++ src/nodes/keyframes.js
@@ -0,0 +1,35 @@
+import Atrule from './atrule'
+
+export default class Keyframes extends Atrule {
+  constructor (segs, prefix) {
+    super('keyframes')
+    this.segments = segs
+    this.prefix = prefix || 'official'
+  }
+
+  clone (parent) {
+    const { block, lineno, column, prefix, segments, filename } = this
+    const keyframes = new Keyframes()
+    return Object.assign(keyframes, {
+      block: block.clone(parent, keyframes),
+      lineno,
+      column,
+      prefix,
+      segments: segments.map(node => node.clone(parent, keyframes)),
+      filename
+    })
+  }
+
+  toJSON () {
+    const { block, lineno, column, prefix, segments, filename } = this
+    return {
+      block,
+      lineno,
+      column,
+      prefix,
+      segments,
+      filename,
+      __type: 'Keyframes'
+    }
+  }
+}
